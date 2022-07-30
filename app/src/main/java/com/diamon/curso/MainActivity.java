bc4216f706626f44b5b6bddf8e07dcd03af95bdf
Danielk10 <danielpdiamon@gmail.com>
2022-07-30 11:52:33 -0430

    Google

--- app/src/main/java/com/diamon/curso/MainActivity.java
+++ app/src/main/java/com/diamon/curso/MainActivity.java
@@ -2,9 +2,6 @@ package com.diamon.curso;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.ads.MobileAds;
-import com.google.android.gms.ads.initialization.InitializationStatus;
-import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
 
 import android.os.Bundle;
@@ -16,11 +13,6 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		 MobileAds.initialize(this, new OnInitializationCompleteListener() {
-            @Override
-            public void onInitializationComplete(InitializationStatus initializationStatus) {
-            }
-        });
 
 	}
 }
