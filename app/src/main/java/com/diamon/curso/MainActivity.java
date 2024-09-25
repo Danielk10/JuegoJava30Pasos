02dbcb27e10482291d353c29b814db55b7e1c6e7
Danielk10 <danielpdiamon@gmail.com>
2024-09-25 11:55:13 -0400

    Nueva versión de App, para proyecto de Android Studio última versión 2024 y corregida para AppCenter

--- app/src/main/java/com/diamon/curso/MainActivity.java
+++ app/src/main/java/com/diamon/curso/MainActivity.java
@@ -7,12 +7,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        AppCenter.start(getApplication(), "cf7ac082-49cd-4cef-bd2d-3f1a3377efa9",
+                  Analytics.class, Crashes.class);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
