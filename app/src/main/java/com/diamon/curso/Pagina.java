012d601067e5ea698d3ddfd5ecb499f7fab187d0
Danielk10 <danielpdiamon@gmail.com>
2024-09-25 20:17:24 -0400

    update

--- app/src/main/java/com/diamon/curso/Pagina.java
+++ app/src/main/java/com/diamon/curso/Pagina.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package com.diamon.curso;
 
 import com.diamon.publicidad.MostrarPublicidad;
--- app/src/main/java/com/diamon/publicidad/MostrarPublicidad.java
+++ app/src/main/java/com/diamon/publicidad/MostrarPublicidad.java
@@ -2,8 +2,6 @@ package com.diamon.publicidad;
 
 import android.app.Activity;
 
-import com.startapp.sdk.adsbase.StartAppAd;
-import com.startapp.sdk.adsbase.StartAppSDK;
 
 public class MostrarPublicidad implements Publicidad {
 
@@ -17,28 +15,16 @@ public class MostrarPublicidad implements Publicidad {
 
 	public static void IniciarPublicidad(Activity actividad) {
 
-		StartAppSDK.init(actividad, Publicidad.ID_DE_APLICACION, Publicidad.ANUNCIOS_DE_DEVOLUCION);
-
-		StartAppAd.disableSplash();
-
-		StartAppAd.disableAutoInterstitial();
 
 	}
 
 	@Override
 	public void mostrarInterstitial() {
 
-		// StartAppAd.disbleAutoInterstitial();
-
-		StartAppAd.showAd(actividad);
-
-		// StartAppAd.enableAutoInterstitial();
-
 	}
 
 	@Override
 	public void botonAtrasInterstitial() {
-		StartAppAd.onBackPressed(actividad);
 
 	}
 
